ef8947ce94b17c406038f2a1f20fbecc80384745
Henri-Mayeul de Benque <henri-mayeul.debenque@doctolib.com>
2025-08-06 14:23:56 +0200

    commit last changes

---
+++ src/aoc_2024/day12.rs
@@ -0,0 +1,110 @@
+use crate::aoc_2024::common::{CharMatrix, Coordinates, Direction};
+#[cfg(not(test))]
+use log::{info, warn};
+// Use log crate when building application
+use std::collections::HashSet;
+use std::str::FromStr;
+#[cfg(test)]
+use std::{println as info, println as warn};
+
+pub fn day10(input: &String) -> String {
+    let char_matrix = CharMatrix::from_str(input).unwrap();
+    info!("Parsed input:\n{}", char_matrix.print());
+
+    String::from("NYI")
+}
+
+pub fn day10_2(input: &String) -> String {
+    let char_matrix = CharMatrix::from_str(input).unwrap();
+    info!("Parsed input:\n{}", char_matrix.print());
+    String::from("NYI")
+}
+
+fn list_region(map: &CharMatrix) {
+    
+}
+
+fn compute_region_fencing_price(region: Vec<Coordinates>) {
+
+}
+
+
+
+fn explore(map: &CharMatrix, previous_level: isize, location: &Coordinates) -> Vec<Coordinates> {
+    let level_opt = map.get_char_at(location);
+    if level_opt.is_err() {
+        return vec![];
+    }
+    let level = char::to_digit(level_opt.unwrap(), 10).unwrap() as isize;
+    if level != previous_level + 1 {
+        return vec![];
+    }
+    println!(
+        "Exploring level (prev: {}) {} at {:?}",
+        previous_level, level, location
+    );
+
+    if level == 9 {
+        return vec![location.clone()];
+    }
+
+    explore(map, level, &Direction::North.advance(location))
+        .into_iter()
+        .chain(explore(map, level, &Direction::East.advance(location)).into_iter())
+        .chain(explore(map, level, &Direction::South.advance(location)).into_iter())
+        .chain(explore(map, level, &Direction::West.advance(location)).into_iter())
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const PUZZLE_INPUT_SMALL: &'static str = "OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO
+";
+    const PUZZLE_INPUT_BIG: &'static str = "RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE
+";
+
+    #[test]
+    #[test_log::test]
+    fn test_day10_small() {
+        let input = String::from(PUZZLE_INPUT_SMALL);
+
+        let result = day10(&input);
+
+        assert_eq!(String::from("140"), result);
+    }
+
+    #[test]
+    #[test_log::test]
+    fn test_day10_big() {
+        let input = String::from(PUZZLE_INPUT_BIG);
+
+        let result = day10(&input);
+
+        assert_eq!(String::from("1930"), result);
+    }
+
+    #[test]
+    #[test_log::test]
+    fn test_day10_2() {
+        let input = String::from(PUZZLE_INPUT_BIG);
+
+        let result = day10_2(&input);
+
+        assert_eq!(String::from("81"), result);
+    }
+}
---
+++ src/aoc_2024/day13.rs
@@ -0,0 +1,132 @@
+use crate::aoc_2024::common::{CharMatrix, Coordinates, Direction, Vector};
+#[cfg(not(test))]
+use log::{info, warn};
+// Use log crate when building application
+use std::collections::HashSet;
+use std::str::FromStr;
+#[cfg(test)]
+use std::{println as info, println as warn};
+
+pub fn day13(input: &String) -> String {
+    let machines = parse_machines(input);
+    info!("Parsed input:\n{:?}", machines);
+    machines.iter().filter_map(Machine::solve).sum::<usize>().to_string()
+}
+
+pub fn day13_2(input: &String) -> String {
+    let char_matrix = CharMatrix::from_str(input).unwrap();
+    info!("Parsed input:\n{}", char_matrix.print());
+    String::from("NYI")
+}
+
+fn parse_machines(input: &String) -> Vec<Machine> {
+    input
+        .split("\n\n")
+        .map(Machine::from_str)
+        .map(Result::unwrap)
+        .collect()
+}
+
+#[derive(Debug, Clone)]
+struct Machine {
+    a_button: Vector,
+    b_button: Vector,
+    prize_loc: Coordinates,
+}
+
+impl FromStr for Machine {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split("\n");
+
+        let vector_a = Self::parse_button(split.next().unwrap());
+        let vector_b = Self::parse_button(split.next().unwrap());
+
+        let prize = split.next();
+        let mut prize_iter = prize.unwrap().split(' ').skip(1);
+        let prize_x: isize = prize_iter.next().unwrap()[2..].strip_suffix(",").unwrap().parse().unwrap();
+        let prize_y: isize = prize_iter.next().unwrap()[2..].parse().unwrap();
+
+        Ok(Machine::new(
+            vector_a,
+            vector_b,
+            Coordinates::new(prize_x, prize_y),
+        ))
+    }
+    
+}
+
+impl Machine {
+    fn new(a_button: Vector, b_button: Vector, prize_loc: Coordinates) -> Machine {
+        Machine {
+            a_button,
+            b_button,
+            prize_loc,
+        }
+    }
+
+    /// X * a.x + Y * b.x = p.x
+    /// X * a.y + Y * b.y = p.y
+    fn solve(&self) -> Option<usize> {
+        let deter = self.a_button.x * self.b_button.y - self.b_button.x * self.a_button.y;
+        if deter == 0 {
+            return None;
+        }
+        let X = (self.prize_loc.x * self.b_button.y - self.prize_loc.y * self.b_button.x) / deter;
+        let X_rem =
+            (self.prize_loc.x * self.b_button.y - self.prize_loc.y * self.b_button.x) % deter;
+        if X_rem != 0 {
+            return None;
+        }
+        let Y = (self.prize_loc.y * self.a_button.x - self.prize_loc.x * self.a_button.y) / deter;
+        let Y_rem =
+            (self.prize_loc.y * self.a_button.x - self.prize_loc.x * self.a_button.y) % deter;
+        if Y_rem != 0 {
+            return None;
+        }
+        Some(X as usize * 3 + Y as usize)
+    }
+
+    fn parse_button(button_raw: &str) -> Vector {
+        let mut split_a = button_raw.split(" ").skip(2);
+        let option = split_a.next();
+        let split_a_x_str = option.unwrap().strip_suffix(",").unwrap();
+        let split_a_x: isize = split_a_x_str[2..].parse().unwrap();
+        let split_a_y_str = split_a.next().unwrap();
+        let split_a_y: isize = split_a_y_str[2..].parse().unwrap();
+        Vector::new(split_a_x, split_a_y)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const PUZZLE_INPUT_SMALL: &'static str = "Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279
+";
+
+    #[test]
+    #[test_log::test]
+    fn test_day13_small() {
+        let input = String::from(PUZZLE_INPUT_SMALL);
+
+        let result = day13(&input);
+
+        assert_eq!(String::from("140"), result);
+    }
+}
