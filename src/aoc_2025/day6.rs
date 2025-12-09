ad9c518d681ce58e568b1e661052a03f49f6b219
Henri-Mayeul de Benque <henri-mayeul.debenque@doctolib.com>
2025-12-09 20:45:09 +0100

    day 6, part 2

--- src/aoc_2025/day6.rs
+++ src/aoc_2025/day6.rs
@@ -3,40 +3,45 @@ use log::info;
 #[cfg(test)]
 use std::println as info;
 use std::str::FromStr;
-use crate::aoc_2025::day6::Operation::{Add, Divide, Multiply, Subtract};
+use crate::aoc_2025::day6::Operator::{Add, Divide, Multiply, Subtract};
 
 pub fn day6(input: &String) -> String {
     parse_input(input).iter()
-        .map(|(values, operation)| {
-            let res = operation.execute_list(values);
-            info!("Executing operation {operation:?} of {values:?} = {res}");
-            res
-        })
+        .map(|operation| operation.compute())
         .sum::<usize>()
         .to_string()
 }
 
 pub fn day6_2(input: &String) -> String {
-    parse_input(input)
-        .iter()
-        .map(|(values, operation)| {
-            let res = operation.execute_list(values);
-            info!("Executing operation {operation:?} of {values:?} = {res}");
-            res
-        })
+    parse_input_2(input).iter()
+        .map(|operation| operation.compute())
         .sum::<usize>()
         .to_string()
 }
 
 #[derive(Debug)]
-enum Operation {
+struct Operation {
+    values: Vec<usize>,
+    operator: Operator
+}
+
+impl Operation {
+    pub fn compute(&self) -> usize {
+        let res = self.operator.execute_list(&self.values);
+        info!("Executing operation {self:?}= {res}");
+        res
+    }
+}
+
+#[derive(Debug)]
+enum Operator {
     Add,
     Subtract,
     Multiply,
     Divide
 }
 
-impl Operation {
+impl Operator {
 
     fn get_neutral(&self) -> usize {
         match self {
@@ -61,7 +66,7 @@ impl Operation {
     }
 }
 
-impl FromStr for Operation {
+impl FromStr for Operator {
     type Err = ();
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
@@ -75,44 +80,79 @@ impl FromStr for Operation {
     }
 }
 
-fn parse_input(input: &str) -> Vec<(Vec<usize>, Operation)> {
-    let lines: Vec<Vec<&str>> = input.split("\n")
-        .filter(|line| !line.is_empty())
-        .map(|line| {
-            line.split(" ").map(str::trim).filter(|elem| !elem.is_empty()).collect()
-        })
-        .collect();
-
-    let number_of_operations = lines.first().unwrap().len();
-
-    let mut operations: Vec<(Vec<usize>, Operation)> = Vec::with_capacity(number_of_operations);
-    let (operator_line, values_lines) = lines.split_last().unwrap();
-
-    for op_id in 0..number_of_operations {
-        let operation_values = values_lines.iter()
-            .map(|value_line| value_line.get(op_id).unwrap())
-            .map(|x1| x1.parse::<usize>().unwrap())
-            .collect();
-
-        let operation = operator_line.get(op_id).map(|t| {
-            Operation::from_str(*t).unwrap()
-        }).unwrap();
-
-        operations.push((operation_values, operation))
-    }
-    operations
+fn parse_input(input: &str) -> Vec<Operation> {
+    let split = parse_columns(input);
+    split.iter()
+        .map(|x| to_operation(x))
+        .collect()
 }
 
+fn to_operation(col: &Vec<&str>) -> Operation {
+    let (operator_str, values_str) = col.split_last().unwrap();
+    let values: Vec<usize> = values_str.iter().map(|x| x.trim().parse().unwrap()).collect();
+    let operator = Operator::from_str(operator_str).unwrap();
+    Operation {values, operator }
+}
 
+fn parse_input_2(input: &str) -> Vec<Operation> {
+    let split = parse_columns(input);
+    split.iter()
+        .map(|x| to_operation_2(x))
+        .collect()
+}
+
+fn to_operation_2(col: &Vec<&str>) -> Operation {
+    let (operator_str, values_str) = col.split_last().unwrap();
+
+    let col_len = values_str.first().unwrap().len();
+    let mut values: Vec<usize> = Vec::new();
+
+    for i in (0..col_len).rev() {
+        let value: String = values_str.iter().map(|x| x.get(i..=i).unwrap()).collect();
+        let result = value.trim().parse();
+        if result.is_ok() {
+            values.push(result.unwrap())
+        }
+    }
+
+    let operator = Operator::from_str(operator_str).unwrap();
+
+    Operation {values, operator }
+}
+
+fn parse_columns(input: &str) -> Vec<Vec<&str>> {
+    let split: Vec<&str> = input.split("\n").filter(|x| !x.is_empty()).collect();
+    let nb_of_columns = split.first().unwrap().len();
+
+    let mut columns: Vec<Vec<&str>> = Vec::new();
+    let mut last_index = 0;
+    for i in 0..nb_of_columns {
+        let should_new_col = split.iter().all(|l| l.get(i..=i).unwrap().eq(" "));
+
+        if should_new_col {
+            let new_col = split.iter().map(|l| l.get(last_index..i).unwrap()).collect();
+            info!("New column parsed: {new_col:?}");
+            columns.push(new_col);
+            last_index = i;
+        }
+    }
+    // push last column
+    let new_col = split.iter().map(|l| l.get(last_index..).unwrap()).collect();
+    info!("New column parsed: {new_col:?}");
+    columns.push(new_col);
+    columns
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const TEST_INPUT: &'static str = "123 328  51 64
- 45 64  387 23
-  6 98  215 314
-*   +   *   +
+    const TEST_INPUT: &'static str =
+"\n
+\n123 328  51 64 \
+\n 45 64  387 23 \
+\n  6 98  215 314\
+\n*   +   *   +  \
 ";
 
     #[test]
@@ -131,6 +171,6 @@ mod tests {
 
         let result = day6_2(&input);
 
-        assert_eq!(String::from("14"), result);
+        assert_eq!(String::from("3263827"), result);
     }
 }
